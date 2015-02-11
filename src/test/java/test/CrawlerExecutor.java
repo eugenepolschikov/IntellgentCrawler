1312d15edfe07e12ad94925c14c3bd13749586f1
eugene <eugene.polschikov@gmail.com>
2015-02-12 00:11:17 +0300

    base Setup is accomplished

--- src/test/java/test/CrawlerExecutor.java
+++ src/test/java/test/CrawlerExecutor.java
@@ -1,7 +1,56 @@
 package test;
+import helpers.DriverInit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.*;
+import testlogic.BaseAlgorithm;
+import testlogic.BaseTest;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 
 /**
  * Created by dell on 11.02.2015.
  */
 public class CrawlerExecutor {
+    private WebDriver driver;
+//    private LoginPage loginPage;
+
+
+    private BaseAlgorithm crawlerLogic;
+
+
+    @BeforeClass
+    public void initDriver() throws MalformedURLException {
+        this.driver = DriverInit.driverSetUp(this.driver);
+//        loginPage = PageFactory.initElements(this.driver, LoginPage.class);
+
+
+        crawlerLogic  = new BaseAlgorithm(driver);
+
+
+    }
+
+
+    @BeforeMethod
+    public void webAdminLoginAndEditVenueNavigate() throws IOException, InterruptedException {
+
+
+        driver.get(crawlerLogic.getEnvironmentData().getLoginbaseUrl());
+
+
+    }
+
+    @Test
+    public void navigateToAnotherOneTargetSite(){
+        driver.get("https://www.google.com");
+    }
+
+
+    @AfterClass
+    public void shutDown(){
+        crawlerLogic.driverShutDown(driver);
+    }
+
 }
